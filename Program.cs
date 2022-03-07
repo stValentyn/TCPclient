661f85c747570ef90f369833deb0c9ce486881a3
stValentyn <valik7km@gmail.com>
2022-03-07 19:47:11 +0200

    created solution

--- Program.cs
+++ Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TCPclient
+namespace tcpclient
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var tcp = new TcpClient("localhost", 9091);
+
+            tcp.Connect("localhost", 9090);
+            tcp.GetStream().Write(Encoding.ASCII.GetBytes("Hello TCP listener!"),32,32);
+
+            tcp.Close();
         }
     }
 }
